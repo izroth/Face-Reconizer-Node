2075caebcfb103b57476cb3baeee9900904855ef
izroth <lakhansharma889027@gmail.com>
2023-08-21 19:37:25 +0530

    changes added

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const express = require('express');
-//npm i face-api.js
-
 const app = express();
+
+const { loadWeights } = require('./weightsLoader');
+const { getFaceImage } = require('./faceImageProcessor');
+const { getFaceDetection } = require('./faceDetectionProcessor');
+
+app.get('/loadWeights', async (req, res) => {
+    await loadWeights();
+    res.send('Weights loaded successfully.');
+});
+
+app.get('/processFaceImage', (req, res) => {
+    // Assuming you have an image in req.query.img
+    // You need to pass the 'detections' parameter here
+    // Example: getFaceImage(req.query.img, detections);
+    getFaceImage(req.query.img, detections);
+    res.send('Face image processed successfully.');
+});
+
+app.get('/detectFace', async (req, res) => {
+    // Assuming you have an image in req.query.img
+    await getFaceDetection(req.query.img);
+    res.send('Face detection completed.');
+});
+
 app.listen(3000, () => console.log('Server running on port 3000'));
